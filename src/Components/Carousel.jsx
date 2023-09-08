87bbddb0ad5f9529dc879f7135f1d0d853057f9d
Profir Mihai <profirmihai96@gmail.com>
2023-09-08 13:43:31 +0300

    change test images with real project screenshots + add a favicon

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Style/flickity.css';
 import Flickity from 'react-flickity-component';
-import test1 from '../Assets/test1.jpg';
-import test2 from '../Assets/test2.png';
-import test3 from '../Assets/test3.jpg';
+import pic1 from '../Assets/1.1.png';
+import pic2 from '../Assets/1.2.png';
+import pic3 from '../Assets/2.1.png';
+import pic4 from '../Assets/2.2.png';
+import pic5 from '../Assets/3.1.png';
+import pic6 from '../Assets/3.2.png';
+import pic7 from '../Assets/4.png';
+import pic8 from '../Assets/5.png';
+import pic9 from '../Assets/6.png';
 
 const flickityOptions = {
     initialIndex: 2
@@ -19,9 +25,15 @@ function Carousel() {
       reloadOnUpdate // default false
       static // default false
     >
-      <img alt='img1' src={test1} />
-      <img alt='img2' src={test2} />
-      <img alt='img3' src={test3} />
+      <img alt='img1' src={pic1} />
+      <img alt='img2' src={pic2} />
+      <img alt='img3' src={pic3} />
+      <img alt='img3' src={pic4} />
+      <img alt='img3' src={pic5} />
+      <img alt='img3' src={pic6} />
+      <img alt='img3' src={pic7} />
+      <img alt='img3' src={pic8} />
+      <img alt='img3' src={pic9} />
     </Flickity>
   )
 }
