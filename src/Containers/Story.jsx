218aeca6fb181d14db8c888a0f09491f3eb4f1a1
Profir Mihai <profirmihai96@gmail.com>
2023-09-11 18:23:07 +0300

    add coming soon info at  work and story page

--- src/Containers/Story.jsx
+++ src/Containers/Story.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Style/Story.scss";
+import Footer from "./Footer";
 
 const Story = () => {
   return (
-    <div>Story</div>
-  )
-}
+    <div>
+      <div className="container">
+        <div className="text">
+          <div className="cs-text">Story page is <br/> coming soon... </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Story;
--- src/Containers/Work.jsx
+++ src/Containers/Work.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Style/Work.scss";
+import Footer from "./Footer";
 
 const Work = () => {
   return (
-    <div>Work</div>
-  )
-}
+    <div>
+      <div className="container">
+        <div className="text">
+          <div className="cs-text">Work page is <br/> coming soon...</div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Work;
